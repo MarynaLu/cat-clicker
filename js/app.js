247695174a9b086ec386df6d8dce247cf9da41a6
Maryna-Lus <m.lus@smartassistant.com>
2018-06-03 18:49:56 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var counter1 = 0;
+var counter2 = 0;
+
+function countClicks(){
+
+	function increaseClicks(updateText, theCounter){
+		console.log("Image click works!");
+		theCounter += 1;
+		updateText.innerHTML = theCounter + " clicks";
+	};
+
+	if(event.target === document.getElementsByClassName("theCat")[0]){
+		increaseClicks(document.getElementById("counter1"), counter1);
+	} else if (event.target === document.getElementsByClassName("theCat")[1]){
+		increaseClicks(document.getElementById("counter2"), counter2);
+	};
+};
+
+document.getElementsByClassName("theCat")[0].addEventListener("click", countClicks);
+document.getElementsByClassName("theCat")[1].addEventListener("click", countClicks);
+
+document.getElementsByClassName("catName")[0].innerHTML = "Murchik";
+document.getElementsByClassName("catName")[1].innerHTML = "Murchik's clone ";
